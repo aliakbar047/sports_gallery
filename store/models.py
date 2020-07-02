c52db140a9bd622679a5564566b13d01b7963e28
Akbar Ali <muhammedpa047@gmail.com>
2020-07-02 14:07:34 +0530

    Start Django Project

---
+++ store/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+CATEGORY_CHOICES = (
+    ('sports','SPORTS'),
+    ('shoes','SHOES'),
+    ('jerseys','JERSEYS'),
+    ('kids','KIDS'),
+    ('accessories','accessories'),
+)
+
+class Product(models.Model):
+    title = models.CharField(max_length=30)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES ,default='sports')
+    sub_category = models.CharField(max_length=50)
+    price = models.FloatField()
+    image = models.ImageField()
+    description = models.TextField(max_length=250)
+
+    def __str__(self):
+        return self.title
+
